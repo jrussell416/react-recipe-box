272782e3fdade5de6bbd53536f65b6f0deb6d79f
Jennifer Russell <jc.russell@yahoo.com>
2020-10-22 10:43:45 -0400

    initial commit

---
+++ src/ExistingRecipe.js
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./ExistingRecipe.css"
+
+function ExistingRecipe() {
+    return (
+      <div className="container">
+        <h2>New Recipe</h2>
+      </div>
+    );
+  }
+  
+  export default ExistingRecipe;
+
+
+ 
